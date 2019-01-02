9593c4a5a8daed44abd25cc2f9f23e07f8404398
Ukasz <lukaszpietruszla94@gmail.com>
2019-01-02 22:33:54 +0100

    galery ready

--- js.js
+++ js.js
@@ -15,12 +15,35 @@ $("p.readMore").on("click", function(){
   const info = "[data-paragraphInfo=" + $(this).attr("data-info") + "]";
  
   $(about).toggleClass("ReadMore");
+  // $(about).toggleClass("ReadMore");
+
+
+  
+
   console.log(about)
   $(info).toggleClass("ReadMore");
   console.log(info)
 
   
   // console.log(info)
+})
+
+
+$("p.readMore").on("click", function(){
+
+  const readLessAbout = $("p[data-about='add']")
+  
+
+  //if($($readLessAbout).text( "<p>Read More</p>" )){
+
+if(readLessAbout.innerHTML === "Read More"){
+  readLessAbout.innerHTML = "Readddd More"
+  //    $(about).textContent= "<p>Readddd More</p>"
+}
+  else{
+    $("p[data-about='add']").textContent= "<p>Readddd dupa</p>"
+  }
+
 })
 
 // slide section
@@ -57,3 +80,31 @@ else{
 }
 
 $(window).on("scroll", slideFunction);
+
+// image slider
+/*
+Muszę napisać funkcję będzie miała:
+a) tablicę zdjęć
+b) tablicę napisów
+FUNKCJA musi co 2 sekundy zmieniać zdjecie, czyli pobrać z tablicy i wstawić w miejsce starego */
+
+let acviveElement = 0;
+let timeChange = 4000;
+
+const imgHTML = document.querySelector(".mainTitle__slider_img");
+const titleHTML = document.querySelector("[data-title='title']");
+
+const imgTable = ["img/io.jpg", "img/jass.jpg", "img/julek.jpg" ];
+const titleTable = ["img/io.jpg", "img/jass.jpg", "img/julek.jpg" ];
+
+
+function changeElement(){
+  acviveElement++
+  if(acviveElement == imgTable.length){
+    acviveElement = 0;
+  }
+  imgHTML.src = imgTable[acviveElement];
+  titleHTML.textContent = titleTable[acviveElement]
+}
+
+setInterval(changeElement, timeChange)
